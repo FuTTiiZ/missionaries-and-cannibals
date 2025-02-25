dfab9787434fdc1392e2531a645fb2da64f7eb74
FuTTiiZ <benjamin.b.soerensen@gmail.com>
2025-02-25 07:10:15 +0100

    moved stuff to game.svelte.ts

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,3 +2,12 @@ export enum PersonType {
   CANNIBAL = 0,
   MISSIONARY,
 }
+
+export enum BankType {
+  START = 0,
+  END,
+}
+
+export type Bank = [number, number]
+
+export type Move = [number, number]
